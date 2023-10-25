75bf03dd6870556f2363b57cfec64386cc30b81a
Patrick-Moxom <148964331+Patrick-Moxom@users.noreply.github.com>
2023-10-25 11:58:16 +0100

    Player Data Added
    Added player data class

---
+++ src/PlayerData.java
@@ -0,0 +1,14 @@
+public class PlayerData {
+
+    public final String name;
+    public final int points;
+
+    public PlayerData(String name, int points){
+        this.name = name;
+        this.points = points;
+    }
+
+    public String getName() {return name;}
+
+    public int getPoints(){return points;}
+}
