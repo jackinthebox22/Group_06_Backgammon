34cf6f8bc2e4c83963ffe1d252fa0f1489f513ca
Patrick-Moxom <148964331+Patrick-Moxom@users.noreply.github.com>
2023-10-26 16:51:06 +0100

    Changed roll

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ public class Main{
 
     public static void main(String[] args) {
         // Scanner in = new Scanner(System.in); // Allows user to make inputs
-        
+    
         Board board = new Board();
         board.displayBoard();
+        Roll.rollDice;
     }
 }
--- src/Roll.java
+++ src/Roll.java
@@ -1,14 +1,14 @@
-import java.util.Random
+import java.util.Random;
 
 public class Roll{
 
     public int[] rollDice(){ //Roll not Role u dummy
 
-        int die1 = random.nextInt(6) + 1;
-        int die2 = random.nextInt(6) + 1;
-
-        int[] dice = new int[2];
-        dice = {die1, die2};
+        Random rand = new Random();
+        int die1 =rand.nextInt(6) + 1;
+        int die2 = rand.nextInt(6) + 1;
+        System.out.println(die1 + "" + die2);
+        int[] dice = {1, 2};
 
         return dice;
     }
