23b9d37259581be052d47cb302c1a6c40a3046e8
jackinthebox22 <jack.caldwell831@gmail.com>
2023-10-31 10:08:31 +0000

    Created starting Display
    I have made a complete starting screen based off our idea in class. Changed up main so that it can run it. I recon next step would be to make a checkers class rather than me manually seeting the arrays.

--- src/Board.java
+++ src/Board.java
@@ -1,23 +1,71 @@
-import java.util.*;
-
 public class Board {
 
-    // Initializes new Board
+    private String[][] spikes;
+
     public Board() {
+        // Initialize the board with empty spikes
+        spikes = new String[2][12];
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 12; j++) {
+                spikes[i][j] = "";
+            }
+        }
 
+        // Starting Spikes
+        spikes[0][0] = "5O";  // Spike 13
+        spikes[0][4] = "3X";  // Spike 17
+        spikes[0][6] = "5X";  // Spike 19
+        spikes[0][11] = "2O"; // Spike 24
+        spikes[1][0] = "5X";  // Spike 12
+        spikes[1][4] = "3O";  // Spike 8
+        spikes[1][6] = "5O";  // Spike 6
+        spikes[1][11] = "2X"; // Spike 1
     }
-
     // Displays board to user
     public void displayBoard() {
-        String[] checkers = {"2X", "3O", "4X", "1O", "5X", "6O", "7X", "8O", "9X", "1O", "1X", "1O"}; // Example strings
-        System.out.println(" ——————————————————————————————————————————————————————————");
-        System.out.println("| 12   11   10   09   08   07 | |  06    05    04    03    02    01 |");
 
-        System.out.printf("|");
-        for (String checker : checkers) {
-            System.out.printf(" %2s  ", checker); // Use %2s to specify a field width of 2 characters
+        // Print Top of Board (12-24)
+        System.out.println(" \n————————————————————————————————————————————————————————————————");
+        System.out.print("|");
+
+        for (int i = 12; i <= 17; i++) {
+            System.out.printf(" %2s |", i + 1); 
         }
-        System.out.println("|"); // Print a new line at the end of the row
+        System.out.print("  |");
+
+        for (int i = 18; i <= 23; i++) {
+            System.out.printf(" %2s |", i + 1); 
+        }
+
+        // Print 2D array of checkers
+        for (int i = 0; i < 2; i++) {
+            if (i == 0) {
+                System.out.print("\n");
+            }
+            if (i == 1) {
+                System.out.println("————————————————————————————————————————————————————————————————");
+            }
+            System.out.print("|");
+            for (int j = 0; j <= 11; j++) {
+                if (j == 6) {
+                    System.out.print("  |");
+                }
+                System.out.printf(" %-3s|", spikes[i][j]); // Spike content with 4-character width
+            }
+            System.out.println();
+        }
+
+        // Print bottom half of board
+        System.out.print("|");
+        for (int i = 11; i >= 6; i--) {
+            System.out.printf(" %2s |", i + 1); // Print spike numbers with 2-character width
+        }
+        System.out.print("  |");
+        for (int i = 5; i >= 0; i--) {
+            System.out.printf(" %2s |", i + 1); // Print spike numbers with 2-character width
+        }
+
+        System.out.println("\n————————————————————————————————————————————————————————————————\n");
     }
 
     public static void main(String[] args) {
