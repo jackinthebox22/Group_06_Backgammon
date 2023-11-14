078d87b8e8aa70337f21b227cc2fea0c91abd210
Patrick-Moxom <148964331+Patrick-Moxom@users.noreply.github.com>
2023-11-14 12:57:10 +0000

    Merge branch 'main' of https://github.com/jackinthebox22/Backgammon

--- src/ValidMoves.java
+++ src/ValidMoves.java
@@ -5,24 +5,29 @@ public class ValidMoves {
         ArrayList<ArrayList<Integer>> allMoves = new ArrayList<>();
         int allmovesindex = 0;
         int spikenumber = 0;
+
         for (int row = 1; row >= 0; row--){
             for (int col = 11; col >= 0; col--){
 
                 if(spikes[row][col] != null && spikes[row][col].getColour().equals(colour)){
                     for(int i = 0; i <= 2; i++){
                         spikenumber = Board.convertIndicesToSpike(row, col);
+
                         ArrayList<Integer> allmovesrow = new ArrayList<>();
-                        allmovesrow.add(allmovesindex++);
-                        allmovesrow.add(spikenumber);
 
-                        int moveto = spikenumber + dice[0] + dice[1];
+                        int moveto = spikenumber + (dice[0] + dice[1])*direction;
 
-                        if (i < 2) moveto = spikenumber + dice[i];
+                        if (i < 2) moveto = spikenumber + dice[i]*direction;
 
-                        if (moveto < 1 || moveto > 24) moveto = 0;
-                        allmovesrow.add(moveto);
+                        if (moveto == 25) moveto = 0;
 
-                        allMoves.add(allmovesrow);
+                        if(moveto >= 0 && moveto <= 24) {
+                            allmovesrow.add(allmovesindex++);
+                            allmovesrow.add(spikenumber);
+                            allmovesrow.add(moveto);
+
+                            allMoves.add(allmovesrow);
+                        }
                     }
                 }
             }
