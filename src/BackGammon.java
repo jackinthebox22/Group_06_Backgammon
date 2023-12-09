5a30db239f92b21ef022306d30122f5e7d415604
jackinthebox22 <jack.caldwell831@gmail.com>
2023-12-09 14:15:19 +0000

    Plz Work

--- src/BackGammon.java
+++ src/BackGammon.java
@@ -347,9 +347,6 @@ public class BackGammon {
                                             System.out.println("No more moves. Ending " + player[currentPlayer].getName() + "'s turn.");
                                         } else if (barMoves.isEmpty() && !allMoves.isEmpty()) {
                                             Board.displayBoard(spikes, tray, player[currentPlayer], bar);
-                                            if (bar.hasCheckersOfColor(player[currentPlayer].getPlayerColour()))
-                                                nextPlayerTurn = true;
-
                                             movesMade++;
                                             break;
                                         } else {
